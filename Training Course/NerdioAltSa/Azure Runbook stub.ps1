83f60852105ab8263743a09599ca93e16af34b5d
Travis Lamming <tlamming@qualitech.net>
2024-07-11 11:31:46 -0400

    init2

---
+++ Training Course/NerdioAltSa/Azure Runbook stub.ps1
@@ -0,0 +1,36 @@
+#description: A runbook script
+#tags: Preview
+
+<# Notes:
+    This script will do things in Azure!
+#>
+
+<# Variables:
+{
+    "Param1": {
+        "Description": "The first parameter",
+        "IsRequired": true
+    },
+    "Param2": {
+        "Description": "An optional parameter",
+        "IsRequired": false
+    }
+}
+#>
+
+$ErrorActionPreference = 'Stop'
+
+
+# Retrieve Host Pool session hosts
+
+
+# Parse VM names from session host names
+
+
+# Set WAP_SCALE_IN_RESTRICTION tag on each VM to future datetime based on Hours parameter (See lab guide for hint)
+
+
+# Start VMs 
+
+
+
